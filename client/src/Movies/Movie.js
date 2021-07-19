9e205210b400b535ca5d1a39144d11acd2b8a015
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-07-19 16:00:33 -0700

    Finished MVPs

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
-
-  let id = 1;
+  console.log(movie)
+  const {movies}=props
+  const {movieId}=useParams()
+  
+  const {id} = movies.find(item=>item.id == movieId);
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
       .then(response => {
+        setMovie(response.data)
+        
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
       })
@@ -19,7 +26,7 @@ export default function Movie(props) {
       });
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, [id]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
@@ -41,7 +48,7 @@ export default function Movie(props) {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-
+        
         {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
+import Movie from './Movie'
 export default function MovieList(props) {
+  const {movies}=props
+  // console.log(movies)
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link key={movie.id} to={`/movies/${movie.id}`}>
+        <MovieDetails  movie={movie} />
+        </Link>
       ))}
     </div>
   );
@@ -22,6 +27,7 @@ function MovieDetails(props) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+      
     </div>
   );
 }
